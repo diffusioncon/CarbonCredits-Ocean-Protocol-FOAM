dc06b1481ea6a4f2654da817a5b56ebdcceb93c3
Jason Smythe <jason@smythemail.za.net>
2019-10-19 16:38:00 +0200

    Fix migrate function to load all tokens

--- migrations/3_initialize_v0.js
+++ migrations/3_initialize_v0.js
@@ -1,4 +1,5 @@
 const ERC721Patronage_v0 = artifacts.require("ERC721Patronage_v0");
+const ERC20PatronageReceipt_v0 = artifacts.require("ERC20PatronageReceipt_v0");
 const CarbonCreditSteward_v0 = artifacts.require("CarbonCreditSteward_v0");
 
 const patronageNumerator = 2400000000000
@@ -26,9 +27,22 @@ module.exports = function (deployer, networkName, accounts) {
     console.log(await patronageToken.isMinter.call(accounts[0]))
     await Promise.all([
       patronageToken.mintWithTokenURI(steward.address, 0, image1MetadataString, { from: accounts[0] }),
-      patronageToken.mintWithTokenURI(steward.address, 1, image2MetadataString, { from: accounts[0] })
+      patronageToken.mintWithTokenURI(steward.address, 1, image2MetadataString, { from: accounts[0] }),
+      patronageToken.mintWithTokenURI(steward.address, 2, image2MetadataString, { from: accounts[0] }),
+      patronageToken.mintWithTokenURI(steward.address, 3, image2MetadataString, { from: accounts[0] }),
+      patronageToken.mintWithTokenURI(steward.address, 4, image2MetadataString, { from: accounts[0] }),
+      patronageToken.mintWithTokenURI(steward.address, 5, image2MetadataString, { from: accounts[0] }),
+      patronageToken.mintWithTokenURI(steward.address, 6, image2MetadataString, { from: accounts[0] }),
+      patronageToken.mintWithTokenURI(steward.address, 7, image2MetadataString, { from: accounts[0] }),
+      patronageToken.mintWithTokenURI(steward.address, 8, image2MetadataString, { from: accounts[0] }),
     ])
-    await steward.initialize(patronageToken.address, accounts[0], patronageDenominator, { from: accounts[0] })
-    await steward.listNewTokens([0, 1], [accounts[0], accounts[0]], [patronageNumerator, patronageNumerator], { from: accounts[0] })
+    console.log(ERC20PatronageReceipt_v0.address)
+    await steward.initialize(patronageToken.address, accounts[0], patronageDenominator, ERC20PatronageReceipt_v0.address, { from: accounts[0] })
+    await steward.listNewTokens(
+      [0, 1, 2, 3, 4, 5, 6, 7, 8],
+      [accounts[0], accounts[0], accounts[0], accounts[0], accounts[0], accounts[0], accounts[0], accounts[0], accounts[0]],
+      [patronageNumerator, patronageNumerator, patronageNumerator, patronageNumerator, patronageNumerator, patronageNumerator, patronageNumerator, patronageNumerator, patronageNumerator],
+      { from: accounts[0] }
+    )
   })
 }
