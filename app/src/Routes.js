70456b2fba8cddcf5bfb612cb3adf1f2195a7b2f
Jason Smythe <jason@smythemail.za.net>
2019-10-20 01:03:57 +0200

    Add the Dapp to the tab

--- app/src/Routes.js
+++ app/src/Routes.js
@@ -1,39 +1,40 @@
 import React from 'react'
 import {
-    BrowserRouter as Router,
-    Switch,
-    Route,
-    Link
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
 } from "react-router-dom";
 import Home from './pages/Home'
 import OceanProtocol from './ocean-protocol/OceanProtocol'
 import NavBar from './components/NavBar';
+import App from './App'
 
 const Routes = () => {
-    return (
-        <Router>
-            <NavBar/>
-            <div>
-                <Switch>
-                    <Route path="/for-farmers">
-                        <p>For farmers</p>
-                    </Route>
-                    <Route path="/for-carbon-traders">
-                        <p>For Carbon Traders</p>                        
-                    </Route>
-                    <Route path="/ocean-dashboard">
-                        <OceanProtocol/>
-                    </Route>                        
-                    <Route path="/buy-land">
-                        <p>Buy land zone</p>
-                    </Route>                        
-                    <Route path="/">
-                        <Home/>
-                    </Route>                        
-                </Switch>
-            </div>
-        </Router>
-    )
+  return (
+    <Router>
+      <NavBar />
+      <div>
+        <Switch>
+          <Route path="/for-farmers">
+            <p>For farmers</p>
+          </Route>
+          <Route path="/for-carbon-traders">
+            <p>For Carbon Traders</p>
+          </Route>
+          <Route path="/ocean-dashboard">
+            <OceanProtocol />
+          </Route>
+          <Route path="/buy-land">
+            <App />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+  )
 }
 
 export default Routes
