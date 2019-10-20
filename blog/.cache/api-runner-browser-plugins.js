cb0f720cb6eca39ed768b3ec62192b365182f497
denhampreen <denhampreen@gmail.com>
2019-10-20 13:39:42 +0200

    blog

--- blog/.cache/api-runner-browser-plugins.js
+++ blog/.cache/api-runner-browser-plugins.js
@@ -1,13 +1,13 @@
 module.exports = [{
-      plugin: require('/Users/johanthan/Code/alwaysforsale/Team-9/blog/node_modules/gatsby-plugin-catch-links/gatsby-browser.js'),
+      plugin: require('/home/denham/Desktop/projects/Team-9/blog/node_modules/gatsby-plugin-catch-links/gatsby-browser.js'),
       options: {"plugins":[]},
     },{
-      plugin: require('/Users/johanthan/Code/alwaysforsale/Team-9/blog/node_modules/gatsby-remark-images/gatsby-browser.js'),
+      plugin: require('/home/denham/Desktop/projects/Team-9/blog/node_modules/gatsby-remark-images/gatsby-browser.js'),
       options: {"plugins":[],"maxWidth":750,"quality":90,"linkImagesToOriginal":true},
     },{
-      plugin: require('/Users/johanthan/Code/alwaysforsale/Team-9/blog/node_modules/gatsby-plugin-typography/gatsby-browser.js'),
+      plugin: require('/home/denham/Desktop/projects/Team-9/blog/node_modules/gatsby-plugin-typography/gatsby-browser.js'),
       options: {"plugins":[],"pathToConfigModule":"config/typography.js"},
     },{
-      plugin: require('/Users/johanthan/Code/alwaysforsale/Team-9/blog/node_modules/gatsby-plugin-offline/gatsby-browser.js'),
+      plugin: require('/home/denham/Desktop/projects/Team-9/blog/node_modules/gatsby-plugin-offline/gatsby-browser.js'),
       options: {"plugins":[]},
     }]
--- blog/.cache/async-requires.js
+++ blog/.cache/async-requires.js
@@ -2,12 +2,12 @@
 const preferDefault = m => m && m.default || m
 
 exports.components = {
-  "component---src-pages-tags-jsx": () => import("/Users/johanthan/Code/alwaysforsale/Team-9/blog/src/pages/tags.jsx" /* webpackChunkName: "component---src-pages-tags-jsx" */),
-  "component---src-templates-tag-jsx": () => import("/Users/johanthan/Code/alwaysforsale/Team-9/blog/src/templates/tag.jsx" /* webpackChunkName: "component---src-templates-tag-jsx" */),
-  "component---src-templates-post-jsx": () => import("/Users/johanthan/Code/alwaysforsale/Team-9/blog/src/templates/post.jsx" /* webpackChunkName: "component---src-templates-post-jsx" */),
-  "component---cache-dev-404-page-js": () => import("/Users/johanthan/Code/alwaysforsale/Team-9/blog/.cache/dev-404-page.js" /* webpackChunkName: "component---cache-dev-404-page-js" */),
-  "component---src-pages-404-jsx": () => import("/Users/johanthan/Code/alwaysforsale/Team-9/blog/src/pages/404.jsx" /* webpackChunkName: "component---src-pages-404-jsx" */),
-  "component---src-pages-blog-jsx": () => import("/Users/johanthan/Code/alwaysforsale/Team-9/blog/src/pages/blog.jsx" /* webpackChunkName: "component---src-pages-blog-jsx" */),
-  "component---src-pages-index-jsx": () => import("/Users/johanthan/Code/alwaysforsale/Team-9/blog/src/pages/index.jsx" /* webpackChunkName: "component---src-pages-index-jsx" */)
+  "component---node-modules-gatsby-plugin-offline-app-shell-js": () => import("/home/denham/Desktop/projects/Team-9/blog/node_modules/gatsby-plugin-offline/app-shell.js" /* webpackChunkName: "component---node-modules-gatsby-plugin-offline-app-shell-js" */),
+  "component---src-pages-tags-jsx": () => import("/home/denham/Desktop/projects/Team-9/blog/src/pages/tags.jsx" /* webpackChunkName: "component---src-pages-tags-jsx" */),
+  "component---src-templates-tag-jsx": () => import("/home/denham/Desktop/projects/Team-9/blog/src/templates/tag.jsx" /* webpackChunkName: "component---src-templates-tag-jsx" */),
+  "component---src-templates-post-jsx": () => import("/home/denham/Desktop/projects/Team-9/blog/src/templates/post.jsx" /* webpackChunkName: "component---src-templates-post-jsx" */),
+  "component---src-pages-404-jsx": () => import("/home/denham/Desktop/projects/Team-9/blog/src/pages/404.jsx" /* webpackChunkName: "component---src-pages-404-jsx" */),
+  "component---src-pages-blog-jsx": () => import("/home/denham/Desktop/projects/Team-9/blog/src/pages/blog.jsx" /* webpackChunkName: "component---src-pages-blog-jsx" */),
+  "component---src-pages-index-jsx": () => import("/home/denham/Desktop/projects/Team-9/blog/src/pages/index.jsx" /* webpackChunkName: "component---src-pages-index-jsx" */)
 }
 
