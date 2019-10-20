0f1c807e6b070909fbd212a786a20701054c1c85
Jason Smythe <jason@smythemail.za.net>
2019-10-20 13:44:41 +0200

    Fix routes broken during merge

--- app/src/Routes.js
+++ app/src/Routes.js
@@ -1,9 +1,9 @@
 import React from 'react'
 import {
-    BrowserRouter as Router,
-    Switch,
-    Route,
-    Link
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
 } from "react-router-dom";
 import Home from './pages/Home'
 import OceanProtocol from './ocean-protocol/OceanProtocol'
@@ -19,69 +19,72 @@ import translations from './translations'
 
 
 const theme = createMuiTheme({
-    palette: {
-        primary: {
-            main: '#3F3D4B'
-        },
-        secondary: {
-            main: '#fff'
-        },
+  palette: {
+    primary: {
+      main: '#3F3D4B'
     },
-    typography: {
-        fontFamily: 'Lato, Helvetica, sans-serif',
-        fontWeight: 700
-    }
+    secondary: {
+      main: '#fff'
+    },
+  },
+  typography: {
+    fontFamily: 'Lato, Helvetica, sans-serif',
+    fontWeight: 700
+  }
 });
 
 console.log(window.location.pathname)
 
 const Routes = () => {
 
-    const [cryptoLanguageMode, setCryptoLanguageMode ] = React.useState(true) 
+  const [cryptoLanguageMode, setCryptoLanguageMode] = React.useState(true)
 
-    const handleLanguageToggle = () => {
-      setCryptoLanguageMode(!cryptoLanguageMode)
-    }
+  const handleLanguageToggle = () => {
+    setCryptoLanguageMode(!cryptoLanguageMode)
+  }
 
-    return (
-        <MuiThemeProvider theme={theme}>
-            <TranslatorProvider translations={translations}>
-                <Router>
-                    <div>
-                        <Switch>
-                            <Route path="/for-farmers">
-                                <ForFarmers />
-                            </Route>
-                            <Route path="/for-carbon-traders">
-                                <ForCarbonTraders />
-                            </Route>
-                            <Route path="/ocean-dashboard">
-                                <OceanProtocol />
-                            </Route>
-                            <Route path="/buy-land">
-                                <App />
-                            </Route>
-                            <Route path="/terms-and-conditions">
-                                <NavBar />
-                                <p style={{ height: '70vh' }}>Terms and conditions page :)</p>
-                            </Route>
-                            <Route path="/privacy-policy">
-                                <NavBar />
-                                <p style={{ height: '70vh' }}>Privacy Policy Page :)</p>
-                            </Route>
-                            <Route path="/">
-                                <Home  
-                                    cryptoLanguageMode={cryptoLanguageMode}
-                                    handleLanguageToggle={handleLanguageToggle}
-                                />
-                            </Route>
-                        </Switch>
-                        <Footer />
-                    </div>
-                </Router>
-            </TranslatorProvider>
-        </MuiThemeProvider>
-    )
+  return (
+    <MuiThemeProvider theme={theme}>
+      <TranslatorProvider translations={translations}>
+        <Router>
+          <div>
+            <Switch>
+              <Route path="/for-farmers">
+                <ForFarmers />
+              </Route>
+              <Route path="/for-carbon-traders">
+                <ForCarbonTraders />
+              </Route>
+              <Route path="/ocean-dashboard">
+                <OceanProtocol />
+              </Route>
+              <Route path="/verify-land">
+                <App verifyPlot={true} />
+              </Route>
+              <Route path="/buy-land">
+                <App />
+              </Route>
+              <Route path="/terms-and-conditions">
+                <NavBar />
+                <p style={{ height: '70vh' }}>Terms and conditions page :)</p>
+              </Route>
+              <Route path="/privacy-policy">
+                <NavBar />
+                <p style={{ height: '70vh' }}>Privacy Policy Page :)</p>
+              </Route>
+              <Route path="/">
+                <Home
+                  cryptoLanguageMode={cryptoLanguageMode}
+                  handleLanguageToggle={handleLanguageToggle}
+                />
+              </Route>
+            </Switch>
+            <Footer />
+          </div>
+        </Router>
+      </TranslatorProvider>
+    </MuiThemeProvider>
+  )
 }
 
 export default Routes
