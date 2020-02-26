c61142e3d65bceef02c4d44ac1d0db3eeeef0d65
denhampreen <denhampreen@gmail.com>
2020-02-26 23:08:38 +0100

    Updates

---
+++ precache-manifest.bac2ecd935ee7ef0e8c9830e76254c75.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "65c4e664719e45af8187",
+    "url": "/static/css/main.6cdc00be.chunk.css"
+  },
+  {
+    "revision": "65c4e664719e45af8187",
+    "url": "/static/js/main.65c4e664.chunk.js"
+  },
+  {
+    "revision": "8396dbe1718bc048d9aa",
+    "url": "/static/css/1.f3dc3126.chunk.css"
+  },
+  {
+    "revision": "8396dbe1718bc048d9aa",
+    "url": "/static/js/1.8396dbe1.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "fdf639224abcf99d1bbf7295407bd975",
+    "url": "/static/media/pin.fdf63922.svg"
+  },
+  {
+    "revision": "65d9a47bbdb829241dba318d6b0abe52",
+    "url": "/static/media/logov1.65d9a47b.png"
+  },
+  {
+    "revision": "3dcaa5ef4201a418e6edc7cc415e6509",
+    "url": "/static/media/map-background.3dcaa5ef.png"
+  },
+  {
+    "revision": "cabeb719638b93e5d24f7e039be24989",
+    "url": "/static/media/Diffusion-2019_logo.cabeb719.png"
+  },
+  {
+    "revision": "5fe0e55f8e19bc4cc3201876dce7b224",
+    "url": "/static/media/spinner.5fe0e55f.gif"
+  },
+  {
+    "revision": "bfb9764b45d0db35791058fa2be6d890",
+    "url": "/static/media/background-layer.bfb9764b.png"
+  },
+  {
+    "revision": "5a907f058d56caf8ae4d9d4cd2deb673",
+    "url": "/static/media/background-layer-reverse.5a907f05.png"
+  },
+  {
+    "revision": "9f3028eab236219186ad27e775cf36c2",
+    "url": "/static/media/background-layer-plant.9f3028ea.png"
+  },
+  {
+    "revision": "d9323dca5fea349af864beffbf71807e",
+    "url": "/index.html"
+  }
+];
